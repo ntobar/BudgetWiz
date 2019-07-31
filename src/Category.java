6ee68cb227ebd23ac5ce661e68e12d6723d6dc68
Nicolas Tobar <tobar.n@husky.neu.edu>
2019-07-31 10:47:26 -0400

    Added different constructors for category

--- src/Category.java
+++ src/Category.java
@@ -8,13 +8,52 @@ public class Category {
   private int lowAlert;
 
 
+  /**
+   * This constructor initializes a Category with just a Name.
+   * @param name - Represents the name of the category
+   */
   public Category(String name) {
     this.name = name;
-    this.maxExpenses = ;
+    this.amount = 0;
+    this.maxExpenses = amount;
+    this.lowAlert = 0;
+
+
+  }
+
+
+  /**
+   * This constructor initializes a Category with a Name and an initial amount
+   * @param name
+   * @param amount
+   */
+  public Category(String name, int amount) {
+    this.name = name;
+    this.amount = amount;
+    this.maxExpenses = amount;
+    this.lowAlert = 0;
 
 
 
   }
+
+
+  /**
+   * This constructor initializes a Category with a Name, an initial amount, and the max expense.
+   * @param name
+   * @param amount
+   * @param maxExpenses
+   */
+  public Category(String name, int amount, int maxExpenses) {
+    this.name = name;
+    this.amount = 0;
+    this.maxExpenses = maxExpenses;
+    this.lowAlert = 0;
+
+
+
+  }
+
 
 
 
