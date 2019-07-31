5817d8de76ae930093c73f13ff475cbd5b64ea4d
Nicolas Tobar <tobar.n@husky.neu.edu>
2019-07-31 10:41:13 -0400

    Started category classes

---
+++ src/Category.java
@@ -0,0 +1,22 @@
+/**
+ * Respresents a Finance Category
+ */
+public class Category {
+  private String name;
+  private int amount;
+  private int maxExpenses;
+  private int lowAlert;
+
+
+  public Category(String name) {
+    this.name = name;
+    this.maxExpenses = ;
+
+
+
+  }
+
+
+
+
+}
